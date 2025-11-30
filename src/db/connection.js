36ff878ef62f91b34a5c49acc1d621731e1a1e70
kyx03 <78730449+kyx03@users.noreply.github.com>
2025-11-30 13:54:20 +0800

    Refactor database connection to use environment variables

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,10 +1,19 @@
+// import pg from "pg";
+// import { DB_CONFIG } from "../config.js";
+
+// const { Pool } = pg;
+
+// export const pool = new Pool(DB_CONFIG);
+
+// export async function query(sql, params) {
+//   return pool.query(sql, params);
+// }
+
+
 import pg from "pg";
-import { DB_CONFIG } from "../config.js";
+import dotenv from "dotenv";
+dotenv.config();
 
-const { Pool } = pg;
-
-export const pool = new Pool(DB_CONFIG);
-
-export async function query(sql, params) {
-  return pool.query(sql, params);
-}
+const connectionString = process.env.DATABASE_URL || process.env.DB_URL;
+export const pool = new pg.Pool({ connectionString, ssl: { rejectUnauthorized: false } });
+export const query = (text, params) => pool.query(text, params);
